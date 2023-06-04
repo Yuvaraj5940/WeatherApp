e8e582c83456cefa9e9d148624db71a52041fe2b
Yuvaraj5940 <114084208+Yuvaraj5940@users.noreply.github.com>
2023-06-04 14:27:47 +0530

    scroolchnge

--- src/components/secpage.tsx
+++ src/components/secpage.tsx
@@ -68,167 +68,161 @@ const MainPage = () => {
   ];
   return (
     <View style={styles.container}>
-      <View style={{width: '100%', height:'100%', flexWrap:'wrap'}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <TextInput
-            style={styles.searcInput}
-            placeholder="Search City"
-            placeholderTextColor="#FFFFFF"
-            onChangeText={setName}
-          />
-          <Pressable>
-            <Text style={{fontSize: 25, color: '#FFFFFF'}}> &deg;C</Text>
-          </Pressable>
-        </View>
-
-        <View style={styles.box1}>
-          <Text style={{color: '#FFFFFF', fontSize: 20}}>
-            {date[0]}, {date[1]} {date[2]}
-          </Text>
-          <Text style={{fontSize: 30, color: '#FFFFFF'}}>{name}</Text>
-          <Text style={{color: '#d1e0e0'}}>India</Text>
-        </View>
-
-        <View style={styles.box2}>
-          <View
-            style={{
-              width: '50%',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: 10,
-            }}>
-            <Text style={{fontSize: 80, color: '#FFFFFF'}}>29&deg;</Text>
-            <Text style={{color: '#d1e0e0'}}>Feels Like 29.9&deg;</Text>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                gap: 10,
-              }}>
-              <View style={{display: 'flex', flexDirection: 'row', gap: 5}}>
-                <Image
-                  source={require('../asects/icons/down-arrow.png')}
-                  style={{height: 17, width: 17, tintColor: '#FFFFFF'}}
-                />
-                <Text style={{color: '#FFFFFF'}}>22.6&deg;</Text>
-              </View>
-              <View style={{display: 'flex', flexDirection: 'row', gap: 5}}>
-                <Image
-                  source={require('../asects/icons/up-arrow.png')}
-                  style={{height: 17, width: 17, tintColor: '#FFFFFF'}}
-                />
-                <Text style={{color: '#FFFFFF'}}>36.8&deg;</Text>
-              </View>
-            </View>
-          </View>
-
-          <View style={{width: '45%', height: '40%', alignItems: 'center'}}>
-            <Image
-              source={require('../asects/icons/partly-cloudy.png')}
-              style={styles.img}
-            />
-          </View>
-        </View>
-
-        <Text
-          style={{
-            color: '#FFFFFF',
-            fontWeight: 'bold',
-            fontSize: 30,
-            textAlign: 'center',
-            margin: 20,
-          }}>
-          Partly cloudy
-        </Text>
-
-        <View
-          style={{borderWidth: 0.5, borderColor: '#d1e0e0', width: '100%'}}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TextInput
+          style={styles.searcInput}
+          placeholder="Search City"
+          placeholderTextColor="#FFFFFF"
+          onChangeText={setName}
         />
-
-        <View style={{display: 'flex', flexDirection: 'row', gap: 20}}>
-          {
-            <FlatList
-              data={hrs}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              renderItem={({item}) => (
-                <View style={{display: 'flex', gap: 10, padding: 20}}>
-                  <Text style={{color: '#d1e0e0'}}>{item.time}</Text>
-                  <Text style={{color: '#FFFFFF', fontSize: 20}}>
-                    {item.deg}&deg;
-                  </Text>
-                </View>
-              )}
-            />
-          }
-        </View>
-
-        <View
-          style={{borderWidth: 0.5, borderColor: '#d1e0e0', width: '100%'}}
-        />
-
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            marginTop: 30,
-            gap: 20,
-            justifyContent: 'space-evenly',
-          }}>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Sunrise</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>05:53 AM</Text>
-          </View>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Wind</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>13 Km/h</Text>
-          </View>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Perciptitation</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>0 mm</Text>
-          </View>
-        </View>
-
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            marginTop: 30,
-            gap: 20,
-            justifyContent: 'space-evenly',
-          }}>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Sunset</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>06:43 PM</Text>
-          </View>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Presure</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>1016 mb</Text>
-          </View>
-          <View style={{display: 'flex', gap: 10}}>
-            <Text style={{color: '#d1e0e0'}}>Humidity</Text>
-            <Text style={{fontSize: 22, color: '#FFFFFF'}}>70 %</Text>
-          </View>
-        </View>
-
-        <Pressable style={{marginTop: 20}} onPress={() => setActive(!active)}>
-          <Image
-            source={require('../asects/icons/up-arrow.png')}
-            style={{
-              height: 25,
-              width: 30,
-              tintColor: '#d1e0e0',
-              alignSelf: 'center',
-            }}
-          />
+        <Pressable>
+          <Text style={{fontSize: 25, color: '#FFFFFF'}}> &deg;C</Text>
         </Pressable>
       </View>
+
+      <View style={styles.box1}>
+        <Text style={{color: '#FFFFFF', fontSize: 20}}>
+          {date[0]}, {date[1]} {date[2]}
+        </Text>
+        <Text style={{fontSize: 30, color: '#FFFFFF'}}>{name}</Text>
+        <Text style={{color: '#d1e0e0'}}>India</Text>
+      </View>
+
+      <View style={styles.box2}>
+        <View
+          style={{
+            width: '50%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 10,
+          }}>
+          <Text style={{fontSize: 80, color: '#FFFFFF'}}>29&deg;</Text>
+          <Text style={{color: '#d1e0e0'}}>Feels Like 29.9&deg;</Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              gap: 10,
+            }}>
+            <View style={{display: 'flex', flexDirection: 'row', gap: 5}}>
+              <Image
+                source={require('../asects/icons/down-arrow.png')}
+                style={{height: 17, width: 17, tintColor: '#FFFFFF'}}
+              />
+              <Text style={{color: '#FFFFFF'}}>22.6&deg;</Text>
+            </View>
+            <View style={{display: 'flex', flexDirection: 'row', gap: 5}}>
+              <Image
+                source={require('../asects/icons/up-arrow.png')}
+                style={{height: 17, width: 17, tintColor: '#FFFFFF'}}
+              />
+              <Text style={{color: '#FFFFFF'}}>36.8&deg;</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{width: '45%', height: '40%', alignItems: 'center'}}>
+          <Image
+            source={require('../asects/icons/partly-cloudy.png')}
+            style={styles.img}
+          />
+        </View>
+      </View>
+
+      <Text
+        style={{
+          color: '#FFFFFF',
+          fontWeight: 'bold',
+          fontSize: 30,
+          textAlign: 'center',
+          margin: 20,
+        }}>
+        Partly cloudy
+      </Text>
+
+      <View style={{borderWidth: 0.5, borderColor: '#d1e0e0', width: '100%'}} />
+
+      <View style={{display: 'flex', flexDirection: 'row', gap: 20}}>
+        {
+          <FlatList
+            data={hrs}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item}) => (
+              <View style={{display: 'flex', gap: 10, padding: 20}}>
+                <Text style={{color: '#d1e0e0'}}>{item.time}</Text>
+                <Text style={{color: '#FFFFFF', fontSize: 20}}>
+                  {item.deg}&deg;
+                </Text>
+              </View>
+            )}
+          />
+        }
+      </View>
+
+      <View style={{borderWidth: 0.5, borderColor: '#d1e0e0', width: '100%'}} />
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: 30,
+          gap: 20,
+          justifyContent: 'space-evenly',
+        }}>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Sunrise</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>05:53 AM</Text>
+        </View>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Wind</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>13 Km/h</Text>
+        </View>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Perciptitation</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>0 mm</Text>
+        </View>
+      </View>
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: 30,
+          gap: 20,
+          justifyContent: 'space-evenly',
+        }}>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Sunset</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>06:43 PM</Text>
+        </View>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Presure</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>1016 mb</Text>
+        </View>
+        <View style={{display: 'flex', gap: 10}}>
+          <Text style={{color: '#d1e0e0'}}>Humidity</Text>
+          <Text style={{fontSize: 22, color: '#FFFFFF'}}>70 %</Text>
+        </View>
+      </View>
+
+      <Pressable style={{marginTop: 20}} onPress={() => setActive(!active)}>
+        <Image
+          source={require('../asects/icons/up-arrow.png')}
+          style={{
+            height: 25,
+            width: 30,
+            tintColor: '#d1e0e0',
+            alignSelf: 'center',
+          }}
+        />
+      </Pressable>
 
       {/* model */}
 
