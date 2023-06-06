6920b398ddc61a323aed240f877c310fc884e568
Yuvaraj5940 <114084208+Yuvaraj5940@users.noreply.github.com>
2023-06-06 12:09:04 +0530

    dinamic

---
+++ src/components/utils/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://api.weatherapi.com/v1/forecast.json?key=43ca3500399e463ba8b101827230506 &q=',
+  timeout: 3000,
+  timeoutErrorMessage: 'Try after some time',
+});
+
+axiosInstance.interceptors.request.use(
+  config =>
+    // Do something before request is sent
+    config,
+  error =>
+    // Do something with request error
+    Promise.reject(error),
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  response =>
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    response,
+  error => {
+    if (error?.response?.data) {
+      return Promise.reject(new Error(error.response.data));
+    }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
